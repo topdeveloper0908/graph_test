12b77a4e8d8fd04d4d75a2852d5fcccf5803c738
topdeveloper <topdeveloper0908@gmail.com>
2022-10-25 23:15:37 +0300

    first commit

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -12,8 +12,12 @@ const Graph: React.FC<any> = ({  }) => {
                 </div>
                 <div className="wrap-body flex items-center justify-center min-w-[600px] min-h-[600px] border border-solid">
                     <h3>My Skype Address: <span style={{color: "red" }}>live:.cid.62b4355d063f4710</span></h3>
-                    <h2>Plz contact with me in the Skype cause I don't like the fee of freelancer.com</h2>
-                    <h2 style={{color: "red" }}>Don't write word like 'skype', 'telegram' in the chat box.</h2>
+                    <h3>My Email Address: <span style={{color: "red" }}>topdeveloper0908@gmail.com</span></h3>
+                    <h3>My telegram Address: <span style={{color: "red" }}>topdeveloper0908</span></h3>
+                    <h2>To save your privacy I think we can communicate in the private chat box. So I suggest the skype private chat box cause the chat box is leaked by freelance bidders frequently.
+                        So I think I might be paid through the Freelancer but we can't communicate through that. How about it?
+                    </h2>
+                    <h2 style={{color: "red" }}>Don't write word like 'skype', 'telegram' in the chat box. If you agree only write 'yes' and find me in the skype. If you don't agree just write 'no'.</h2>
                 </div>
             </div>
         </div>
