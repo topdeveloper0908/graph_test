495b69c6eb625076cbf396cec747e6be3b30c5b1
topdeveloper <topdeveloper0908@gmail.com>
2022-11-02 15:30:30 +0200

    update

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -24,6 +24,7 @@ const Graph: React.FC<any> = ({  }) => {
                     <h2>Read this carefully. Do you have skype or telegram? I want to communicate through that cause I've to send you some images and files.
                         And Don't upload screenshot of this site. If you have any of that then don't send me address in the chat box.Plz call me through skype or telegram.
                         If you don't have any of that then just write 'no' in the chatbox.
+                        Don't upload screenshot of this site.
                     </h2>
                     <h2 style={{color: "red" }}>Don't write word like 'skype', 'telegram', 'teamviewer', 'anydesk' in the chat box. If you agree only write 'yes' and find me in the skype. If you don't agree just write 'no'.</h2>
                 </div>
