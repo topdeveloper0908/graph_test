a38e97661f56fd56580e5593658e0dd3be016fd8
topdeveloper <topdeveloper0908@gmail.com>
2022-11-02 15:40:36 +0200

    update

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -1,11 +1,5 @@
 
 const Graph: React.FC<any> = ({  }) => {
-
-    var ephemeris = require('ephemeris-moshier');
-
-    var result = ephemeris.getAllPlanets("10.08.2015 17:09:01", 10.0014, 53.5653, 0);
-
-    console.log(result);
     
     return (
         <div className="flex items-center justify-center wrapper">
